c39ce6baf8e7950a95b9ab7f3859764be4e7b07f
Nicolas Herschke <nicolas.herschke@gmx.de>
2026-05-01 22:22:32 +0200

    feat: add update autocmd for nvim-treesitter, update plugins

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -54,3 +54,13 @@ autocmd("LspAttach", {
         end
     end,
 })
+
+autocmd("PackChanged", {
+    callback = function(ev)
+        local name, kind = ev.data.spec.name, ev.data.kind
+        if name == "nvim-treesitter" and kind == "update" then
+            if not ev.data.active then vim.cmd.packadd("nvim-treesitter") end
+            vim.cmd("TSUpdate")
+        end
+    end
+})

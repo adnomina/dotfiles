adc587fb5898ca84cbc8338d80a72028bd7f0f1d
Nicolas Herschke <nicolas.herschke@gmx.de>
2026-05-02 19:19:34 +0200

    feat: add config table and useful helpers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,32 @@
-require('vim._core.ui2').enable()
+-- Define config table to be able to pass data between scripts
+-- It is a global variable which can be use both as `_G.Config` and `Config`
+_G.Config = {}
+
+-- Load now to have 'mini.misc' available for custom loading helpers.
+vim.pack.add({ "https://github.com/nvim-mini/mini.nvim" })
+
+-- Loading helpers used to organize config into fail-safe parts.
+local misc = require("mini.misc")
+Config.now = function(f) misc.safely("now", f) end
+Config.later = function(f) misc.safely("later", f) end
+
+-- Define custom autocommand group and helper to create an autocommand.
+local gr = vim.api.nvim_create_augroup("custom-config", {})
+Config.new_autocmd = function(event, pattern, callback, desc)
+    local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
+    vim.api.nvim_create_autocmd(event, opts)
+end
+
+-- Define custom `vim.pack.add()` hook helper. Plugin data is passed as
+Config.on_packchanged = function(plugin_name, kinds, callback, desc)
+    local f = function(ev)
+        local name, kind = ev.data.spec.name, ev.data.kind
+        if not (name == plugin_name and vim.tbl_contains(kinds, kind)) then return end
+        if not ev.data.active then vim.cmd.packadd(plugin_name) end
+        callback(ev.data)
+    end
+    Config.new_autocmd("PackChanged", "*", f, desc)
+end
+
+-- Enable core UI 2
+require("vim._core.ui2").enable()

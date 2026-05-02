6d4702853a2875f82601ab054eaa50b3550b4deb
Nicolas Herschke <nicolas.herschke@gmx.de>
2026-05-02 20:05:46 +0200

    feat: prefer mini plugins

--- .config/nvim/plugin/30_plugins.lua
+++ .config/nvim/plugin/30_plugins.lua
@@ -1,45 +1,22 @@
-vim.pack.add({
-    "https://github.com/catppuccin/nvim",
-    "https://github.com/folke/which-key.nvim",
-    "https://github.com/neovim/nvim-lspconfig",
-    "https://github.com/nvim-tree/nvim-web-devicons",
-    "https://github.com/nvim-treesitter/nvim-treesitter",
-    "https://github.com/mason-org/mason.nvim",
-    "https://github.com/nvim-mini/mini.nvim",
-})
-
 vim.cmd.packadd("nvim.undotree")
 vim.cmd.packadd("nvim.difftool")
 
-require("catppuccin").setup({
-    flavour = "macchiato",
-    transparent_background = false,
-    term_colors = true,
-})
+-- Make concise helpers for installing/adding plugins in two stages
+local add = vim.pack.add
+local now_if_args, later = Config.now_if_args, Config.later
 
--- Apply the Catppuccin colour scheme
-vim.cmd.colorscheme("catppuccin")
+now_if_args(function()
+  -- Define hook to update tree-sitter parsers after plugin is updated
+  local ts_update = function() vim.cmd("TSUpdate") end
+  Config.on_packchanged("nvim-treesitter", { "update" }, ts_update, ":TSUpdate")
 
-require("which-key").setup({
-    preset = "classic",
-    spec = {
-        { "<Leader>f", desc = "+Find" },
-        { "<Leader>e", desc = "+Explore" },
-        {
-            mode = { "n", "x" },
-            { "gO",  desc = "Document Symbols" },
-            { "gra", desc = "Code Action" },
-            { "gri", desc = "Goto Implementation" },
-            { "grn", desc = "Rename" },
-            { "grr", desc = "References" },
-            { "grt", desc = "Goto Type Definition" },
-            { "grx", desc = "Run Code Lens" },
-            { "K",   desc = "Hover" },
-        },
-    },
-})
+  add({
+    "https://github.com/nvim-treesitter/nvim-treesitter",
+    "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
+  })
 
-require("nvim-treesitter").install {
+  -- Define languages which will have parsers installed and auto enabled
+  local languages = {
     "bash",
     "css",
     "diff",
@@ -53,7 +30,6 @@ require("nvim-treesitter").install {
     "json",
     "json5",
     "jsx",
-    "lua",
     "nix",
     "prisma",
     "sql",
@@ -61,22 +37,60 @@ require("nvim-treesitter").install {
     "tsx",
     "typescript",
     "yaml",
-}
+  }
+  local isnt_installed = function(lang)
+    return #vim.api.nvim_get_runtime_file("parser/" .. lang .. ".*", false) == 0
+  end
+  local to_install = vim.tbl_filter(isnt_installed, languages)
+  if #to_install > 0 then require("nvim-treesitter").install(to_install) end
 
-require("mason").setup()
+  -- Enable tree-sitter after opening a file for a target language
+  local filetypes = {}
+  for _, lang in ipairs(languages) do
+    for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
+      table.insert(filetypes, ft)
+    end
+  end
+  local ts_start = function(ev) vim.treesitter.start(ev.buf) end
+  Config.new_autocmd("FileType", filetypes, ts_start, "Start tree-sitter")
+end)
 
--- Fuzzy finder for files, grep, buffers, help, and more
-require("mini.pick").setup()
+-- Language servers ===========================================================
+
+now_if_args(function()
+  add({ 'https://github.com/neovim/nvim-lspconfig' })
+
+  -- Use `:h vim.lsp.enable()` to automatically enable language server based on
+  -- the rules provided by 'nvim-lspconfig'.
+  -- Use `:h vim.lsp.config()` or 'after/lsp/' directory to configure servers.
+  vim.lsp.enable({
+    "bashls",
+    "cssls",
+    "docker-language-server",
+    "eslint",
+    "fish_lsp",
+    "graphql",
+    "html",
+    "jsonls",
+    "lua_ls",
+    "nixd",
+    "postgres_lsp",
+    "prismals",
+    "tailwindcss",
+    "vtsls",
+  })
+end)
+
+-- Color theme ================================================================
+
+now_if_args(function()
+  add({ { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } })
+
+  vim.cmd("color catppuccin-macchiato")
+end)
 
 -- File explorer with column view and preview
 require("mini.files").setup({ windows = { preview = true } })
 
--- Add common bookmarks whenever the explorer opens
-vim.api.nvim_create_autocmd("User", {
-    pattern = "MiniFilesExplorerOpen",
-    callback = function()
-        MiniFiles.set_bookmark("c", vim.fn.stdpath("config"), { desc = "Config" })
-        MiniFiles.set_bookmark("p", vim.fn.stdpath("data") .. "/site/pack/core/opt", { desc = "Plugins" })
-        MiniFiles.set_bookmark("w", vim.fn.getcwd, { desc = "Working directory" })
-    end,
-})
+-- Fuzzy finder for files, grep, buffers, help, and more
+require("mini.pick").setup()

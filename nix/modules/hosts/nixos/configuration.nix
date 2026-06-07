d5db87633f959cbd5af2828100094af9c35cc79e
Nicolas Herschke <nicolas.herschke@gmx.de>
2026-06-07 15:16:56 +0200

    fix: update renamed options

--- nix/modules/hosts/nixos/configuration.nix
+++ nix/modules/hosts/nixos/configuration.nix
@@ -37,16 +37,16 @@
     services.xserver.enable = true;
 
     # Enable the GNOME Desktop Environment.
-    services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = true;
+    services.displayManager.gdm.enable = true;
+    services.desktopManager.gnome.enable = true;
 
     # Configure keymap in X11
     services.xserver.xkb = {
       layout = "us";
       variant = "";
+      options = "ctrl:nocaps";
     };
 
-    services.xserver.xkbOptions = "ctrl:nocaps";
     console.useXkbConfig = true;
 
     # Enable CUPS to print documents.

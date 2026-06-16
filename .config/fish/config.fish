651f66eeb5dcda9228fbda6124c621e4a5a321a9
Nicolas Herschke <nicolas.herschke@gmx.de>
2026-06-16 19:47:02 +0200

    feat: alias claude and opencode to use nono
    Ensure nono is used when using an LLM harness
    
    Clean up and re-order config file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,26 +1,26 @@
-function fish_greeting
-    printf (set_color --bold 00FFFF)"Greetings, User. Welcome to the Grid.\n"(set_color normal)
-end
-
+# Variables
 set -gx EDITOR hx
 set -gx PATH /run/current-system/sw/bin $PATH
-
-alias la="ls -la"
-alias vi='nvim'
-alias vim='nvim'
-alias cc="claude"
-alias oc="opencode"
-alias find="fd"
-
-starship init fish | source
-
-# pnpm
 set -gx PNPM_HOME /Users/nicolas/Library/pnpm
 if not string match -q -- "$PNPM_HOME/bin" $PATH
     set -gx PATH "$PNPM_HOME/bin" $PATH
 end
-# pnpm end
 
-eval "$(fnm env --use-on-cd --version-file-strategy=recursive)"
+# Functions
+function fish_greeting
+    printf (set_color --bold 00FFFF)"Greetings, User. Welcome to the Grid.\n"(set_color normal)
+end
 
+# Aliases
+alias la="ls -la"
+alias vi='nvim'
+alias vim='nvim'
+alias claude="nono run --profile claude-code --allow . -- claude"
+alias opencode="nono run --profile opencode --allow . -- opencode"
+alias cc="claude"
+alias oc="opencode"
+
+# Init scripts
+starship init fish | source
 zoxide init fish | source
+eval "$(fnm env --use-on-cd --version-file-strategy=recursive)"
